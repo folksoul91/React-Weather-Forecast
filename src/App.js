acfacf3a77e9ff5f166882a50fe74cabcaa32b1d
Dane Kim <mr.dane@danes-mbp.lan>
2022-06-07 15:12:35 -0400

    rendering all pages in App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import WeatherForecast from "./weatherForecast";
+import WeatherIcon from "./WeatherIcon";
+import WeatherData from "./WeatherData";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My WeatherIcons App</h1>
+      <WeatherForecast />
+      <WeatherIcon />
+      <WeatherData />
     </div>
   );
 }
